2754bce18a5539c5406d488071e6621c72f54866
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-22 19:27:08 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,16 +18,21 @@ def third_challenge
       ]
    },
    :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
+       :patriarch => {name: "Lord Montague", age: "53"},
+       :matriarch => {name: "Lady Montague", age: "54"},
+       :hero => {name: "Romeo", age: "15", status: "alive"},
+       :hero_friends => [
+           {
+               name: "Benvolio",
+               age: "17",
+               attitude: "worried"
+           },
+           {
+               name: "Mercutio",
+               age: "18",
+               attitude: "hot-headed"
+           }
+       ]
    }
   }
 

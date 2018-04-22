947e44e0c59a071232bc615a374ab683dc74e618
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-22 19:25:46 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,12 +7,12 @@ def third_challenge
       :hero_friends => [
           {
               name: "Benvolio",
-              age: 17,
+              age: "17",
               attitude: "worried"
           },
           {
               name: "Mercutio",
-              age: 18,
+              age: "18",
               attitude: "hot-headed"
           }
       ]

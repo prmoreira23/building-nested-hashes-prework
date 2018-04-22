041337da255fd3d5f2935bf1bfffc419c1b72c94
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-22 19:23:57 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,8 +3,7 @@ def third_challenge
    :montague => {
       :patriarch => {name: "Lord Montague", age: 53},
       :matriarch => {name: "Lady Montague", age: 54},
-      :hero => {name: "Romeo", age: 15, status: "alive"
-       },
+      :hero => {name: "Romeo", age: 15, status: "alive"},
       :hero_friends => [
           {
               name: "Benvolio",
